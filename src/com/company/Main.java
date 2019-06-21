65e2cb3f7444fdfbc56220f187ef59dedaab278f
sotorag <50326275+sotorag@users.noreply.github.com>
2019-06-21 16:43:06 +0300

    Third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) throws EnterException{
         Scanner in= new Scanner(System.in);
-        Vvod a=new Vvod(in.nextLine());
-        a.vvod() ;
+        Input a=new Input(in.nextLine());
+        a.input() ;
         System.out.println(a.vuvod());
     }
 }
